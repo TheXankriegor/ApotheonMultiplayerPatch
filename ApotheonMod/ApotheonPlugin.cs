bf798488396cd8d4bb1bcd154dc547f7b44b3b69
k.cohrs <kiliancohrs@web.de>
2026-06-13 12:32:00 +0200

    feat: patch ApotheonArena networking to connect with Apotheon hosts

--- ApotheonMod/ApotheonPlugin.cs
+++ ApotheonMod/ApotheonPlugin.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Reflection;
+using System.Reflection.Emit;
 
 using BepInEx;
 using BepInEx.NET.Common;
@@ -25,7 +28,13 @@ public class ApotheonPlugin : BasePlugin
 
     #region Fields
 
-    private static Type apotheonGameType;
+    private static ReflectedType netUtilityType;
+    private static ReflectedType netUPnPType;
+    private static ReflectedType netClientType;
+    private static ReflectedType hostInfoType;
+    private static ReflectedType netPeerConfigurationType;
+    private static ReflectedType networkType;
+    private static ReflectedType apotheonGameType;
 
     private static string masterServer;
     private static bool loaded;
@@ -42,20 +51,29 @@ public class ApotheonPlugin : BasePlugin
 
         Log.LogInfo($"Initializing {ModName} v{Version}");
 
-        apotheonGameType = AccessTools.TypeByName("Apotheon.Play.ApotheonGame");
+        apotheonGameType = new ReflectedType("Apotheon.Play.ApotheonGame");
 
-        HarmonyInstance.Patch(AccessTools.Method(apotheonGameType, "NetworkUpdate"),
-                              prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixNetworkUpdate)));
+        HarmonyInstance.Patch(apotheonGameType.Method("NetworkUpdate"), prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixNetworkUpdate)));
 
-        var serverBrowserType = AccessTools.TypeByName("Apotheon.ServerBrowser");
+        var serverBrowserType = new ReflectedType("Apotheon.ServerBrowser");
 
-        HarmonyInstance.Patch(AccessTools.Method(serverBrowserType, "OnInitialize"),
-                              prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixNetworkUpdate)));
+        HarmonyInstance.Patch(serverBrowserType.Method("OnInitialize"), prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixNetworkUpdate)));
     }
 
     #endregion
 
     #region Non-Public Methods
+
+    private static void ReflectApotheonTypes()
+    {
+        networkType = new ReflectedType("Apotheon.Network");
+        hostInfoType = new ReflectedType("Apotheon.HostInfo");
+        netUtilityType = new ReflectedType("Lidgren.Network.NetUtility");
+
+        netPeerConfigurationType = new ReflectedType("Lidgren.Network.NetPeerConfiguration");
+        netClientType = new ReflectedType("Lidgren.Network.NetClient");
+        netUPnPType = new ReflectedType("Lidgren.Network.NetUPnP");
+    }
 
     private static void PrefixNetworkUpdate()
     {
@@ -64,21 +82,34 @@ public class ApotheonPlugin : BasePlugin
 
         try
         {
+            ReflectApotheonTypes();
             InitializeSettings();
 
-            var netUtilityType = AccessTools.TypeByName("Lidgren.Network.NetUtility");
-
-            instance.HarmonyInstance.Patch(AccessTools.Method(netUtilityType, "Resolve", new[]
+            instance.HarmonyInstance.Patch(netUtilityType.Method("Resolve", new[]
             {
                 typeof(string)
             }), prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixResolve)));
-            instance.HarmonyInstance.Patch(AccessTools.Method(netUtilityType, "GetNetworkInterface"),
+            instance.HarmonyInstance.Patch(netUtilityType.Method("GetNetworkInterface"),
                                            postfix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PostfixGetNetworkInterface)));
 
-            instance.HarmonyInstance.Patch(AccessTools.Constructor(apotheonGameType, new[]
+            instance.HarmonyInstance.Patch(AccessTools.Constructor(apotheonGameType.Type, new[]
             {
                 typeof(string), typeof(bool), typeof(string)
             }), prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixDirectJoinConstructor)));
+
+            instance.HarmonyInstance.Patch(AccessTools.Constructor(apotheonGameType.Type, new[]
+            {
+                typeof(string), typeof(bool), typeof(string)
+            }), prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixDirectJoinConstructor)));
+
+            // patches to update ApotheonArena Networking to match Apotheon main game
+            if (AccessTools.AllAssemblies().Any(x => x.GetName().Name == "ApotheonArena"))
+            {
+                instance.HarmonyInstance.Patch(networkType.Method("ClientStart"),
+                                               prefix: new HarmonyMethod(typeof(ApotheonPlugin), nameof(PrefixReplaceClientStart)));
+                instance.HarmonyInstance.Patch(networkType.Method("ClientUpdate"),
+                                               transpiler: new HarmonyMethod(typeof(ApotheonPlugin), nameof(TranspileClientUpdate)));
+            }
 
             instance.Log.LogInfo($"Patched methods.");
             loaded = true;
@@ -86,6 +117,98 @@ public class ApotheonPlugin : BasePlugin
         catch (Exception ex)
         {
             instance.Log.LogError(ex);
+        }
+    }
+
+    private static void PatchPeerDiscovery(object networkInstance)
+    {
+        if (networkType.GetField("NextClientIntroductionRequest", networkInstance) is not > 3.0d)
+            return;
+
+        var client = networkType.GetField("client", networkInstance);
+
+        if (client == null)
+            return;
+
+        var ip = networkType.GetField("IPAddress", networkInstance);
+        var port = networkType.GetField("Port", networkInstance);
+
+        netClientType.Invoke("DiscoverKnownPeer", new[]
+        {
+            typeof(string), typeof(int)
+        }, client, new[]
+        {
+            ip, port
+        });
+    }
+
+    private static IEnumerable<CodeInstruction> TranspileClientUpdate(IEnumerable<CodeInstruction> instructions)
+    {
+        var codes = new List<CodeInstruction>(instructions);
+
+        var nextClientIntroField = networkType.Field("NextClientIntroductionRequest");
+
+        foreach (var code in codes)
+        {
+            yield return code;
+
+            // Detect:
+            // stfld NextClientIntroductionRequest
+            if (code.opcode == OpCodes.Stfld && Equals(code.operand, nextClientIntroField))
+            {
+                yield return new CodeInstruction(OpCodes.Ldarg_0);
+                yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ApotheonPlugin), nameof(PatchPeerDiscovery)));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Replace ApotheonArena ClientStart with the one from the main game
+    /// </summary>
+    private static bool PrefixReplaceClientStart(ref object __instance, ref object host)
+    {
+        try
+        {
+            if (hostInfoType.GetField("ExternalIP", host) is IPEndPoint externalIp)
+            {
+                networkType.SetField("IPAddress", __instance, externalIp.Address.ToString());
+                networkType.SetField("Port", __instance, externalIp.Port);
+            }
+
+            networkType.SetField("HostId", __instance, hostInfoType.GetField("Id", host));
+
+            var config = Activator.CreateInstance(netPeerConfigurationType.Type, "Apotheon");
+            // DiscoveryResponse
+            netPeerConfigurationType.Invoke("EnableMessageType", config, new object[]
+            {
+                64
+            });
+            // NatIntroductionSuccess
+            netPeerConfigurationType.Invoke("EnableMessageType", config, new object[]
+            {
+                2048
+            });
+            netPeerConfigurationType.SetProperty("EnableUPnP", config, true);
+
+            networkType.SetField("config", __instance, config);
+            var client = Activator.CreateInstance(netClientType.Type, config);
+            networkType.SetField("client", __instance, client);
+
+            netClientType.Invoke("Start", client, Array.Empty<object>());
+
+            var upnp = netClientType.GetProperty("UPnP", client);
+            netUPnPType.Invoke("ForwardPort", upnp, new object[]
+            {
+                14242, "Apotheon Arena Game"
+            });
+
+            // skip original
+            return false;
+        }
+        catch (Exception ex)
+        {
+            instance.Log.LogError(ex);
+            return true;
         }
     }
 
